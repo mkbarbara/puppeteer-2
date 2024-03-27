4340d86d561d50ad80beabe5db22c3d87e8d6321
mkbarbara <makarena2001@yandex.ru>
2024-03-27 10:30:26 +0300

    fixing cucumber

--- features/search.feature
+++ features/search.feature
@@ -1,10 +1,7 @@
 Feature: Booking tickets
 
-  Background:
-    Given I navigate to "https://qamid.tmweb.ru/client/index.php"
-    And I wait for the navigation menu to appear
-
   Scenario: Success booking one place
+    Given Navigation menu is loaded
     When I click the second day in the navigation menu
     And I click the first movie seance time
     And I click the first standard chair in the buying scheme
@@ -12,6 +9,7 @@ Feature: Booking tickets
     Then I should see ticket details with cost "150"
 
   Scenario: Success booking VIP place
+    Given Navigation menu is loaded
     When I click the second day in the navigation menu
     And I click the first movie seance time
     And I click the first VIP chair in the buying scheme
@@ -19,6 +17,7 @@ Feature: Booking tickets
     Then I should see ticket details with cost "350"
 
   Scenario: Cant book busy place
+    Given Navigation menu is loaded
     When I click the second day in the navigation menu
     And I click the first taken chair in the buying scheme
     Then the accept button should be disabled
--- features/step_definitions/search.steps.js
+++ features/step_definitions/search.steps.js
@@ -1,45 +1,46 @@
-const { Given, When, Then } = require('@cucumber/cucumber');
+const { Given, When, Then, Before, After } = require("cucumber");
 const { clickElement } = require('./lib/commands.js');
 
-let page;
-
-Given('I navigate to {string}', async (url) => {
-  page = await browser.newPage();
-  await page.setDefaultNavigationTimeout(0);
-  await page.goto(url);
+Before(async function () {
+  const browser = await puppeteer.launch({ headless: false, slowMo: 50 });
+  const page = await browser.newPage();
+  this.browser = browser;
+  this.page = page;
 });
 
-Given('I wait for the navigation menu to appear', async () => {
-  await page.waitForSelector("nav.page-nav");
+After(async function () {
+  if (this.browser) {
+    await this.browser.close();
+  }
+});
+
+Given('Navigation menu is loaded', async function () {
+  return await this.page.waitForSelector("nav.page-nav");
 });
 
 When('I click the second day in the navigation menu', async () => {
-  await clickElement(page, "nav.page-nav .page-nav__day:nth-child(2)");
+  await clickElement(this.page, "nav.page-nav .page-nav__day:nth-child(2)");
 });
 
 When('I click the first movie seance time', async () => {
-  await clickElement(page, ".movie-seances__time:nth-child(1)");
+  await clickElement(this.page, ".movie-seances__time:nth-child(1)");
 });
 
 When('I click the first {string} chair in the buying scheme', async (chairType) => {
-  await clickElement(page, `.buying-scheme__wrapper .buying-scheme__chair_${chairType}:nth-child(1)`);
+  await clickElement(this.page, `.buying-scheme__wrapper .buying-scheme__chair_${chairType}:nth-child(1)`);
 });
 
 When('I click the accept button', async () => {
-  await clickElement(page, ".acceptin-button");
+  await clickElement(this.page, ".acceptin-button");
 });
 
 Then('I should see ticket details with cost {string}', async (expectedCost) => {
-  await page.waitForSelector(".ticket__info .ticket__details", { visible: true });
-  const actual = await page.$eval(".ticket__info .ticket__details", link => link.textContent);
+  await this.page.waitForSelector(".ticket__info .ticket__details", { visible: true });
+  const actual = await this.page.$eval(".ticket__info .ticket__details", link => link.textContent);
   expect(actual).toEqual(expectedCost);
 });
 
 Then('the accept button should be disabled', async () => {
-  const actual = await page.$eval(".acceptin-button", button => button.disabled);
+  const actual = await this.page.$eval(".acceptin-button", button => button.disabled);
   expect(actual).toEqual(true);
-});
-
-After(async () => {
-  await page.close();
 });
